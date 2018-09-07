6355b2757f9e1ac9d82994212ad0f694ce9a5019
NicoleB <Triforce@Nicoles-MacBook-Pro.local>
2018-09-06 18:53:00 -0700

    Pascal name changed to cpp

--- Assignments/ScannerAssignment/wci/frontend/Cpp/tokens/CppErrorToken.cpp
+++ Assignments/ScannerAssignment/wci/frontend/Cpp/tokens/CppErrorToken.cpp
@@ -1,33 +1,34 @@
 /**
- * <h1>PascalErrorToken</h1>
+ * <h1>CppErrorToken</h1>
  *
- * <p>Pascal error token.</p>
+ * <p>Cpp error token.</p>
  *
  * <p>Copyright (c) 2017 by Ronald Mak</p>
  * <p>For instructional purposes only.  No warranties.</p>
  */
-#include "PascalErrorToken.h"
+#include "CppErrorToken.h"
+
 #include "../../Token.h"
-#include "../PascalToken.h"
+#include "../CppToken.h"
 
-namespace wci { namespace frontend { namespace pascal { namespace tokens {
-
-using namespace std;
-using namespace wci::frontend::pascal;
-
-PascalErrorToken::PascalErrorToken(Source *source, PascalErrorCode error_code,
-                                   string token_text)
+namespace wci { namespace frontend { namespace cpp { namespace tokens {
+    
+    using namespace std;
+    using namespace wci::frontend::cpp;
+    
+    CppErrorToken::CppErrorToken(Source *source, CppErrorCode error_code,
+                                 string token_text)
     throw (string)
-    : PascalToken(source)
-{
-    type = (TokenType) PT_ERROR;
-    text = token_text;
-    value = (int) error_code;
-}
-
-void PascalErrorToken::extract() throw (string)
-{
-    // do nothing
-}
-
-}}}}  // namespace wci::frontend::pascal::tokens
+    : CppToken(source)
+    {
+        type = (TokenType) CppT_ERROR;
+        text = token_text;
+        value = (int) error_code;
+    }
+    
+    void CppErrorToken::extract() throw (string)
+    {
+        // do nothing
+    }
+    
+}}}}  // namespace wci::frontend::cpp::tokens
