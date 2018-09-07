15d76ba3a97a2c6f07c599cb4bbbb99a7d760e69
NicoleB <Triforce@Nicoles-MacBook-Pro.local>
2018-09-06 19:35:46 -0700

    Pascal changed to Cpp

--- Assignments/ScannerAssignment/wci/frontend/Cpp/tokens/CppStringToken.cpp
+++ Assignments/ScannerAssignment/wci/frontend/Cpp/tokens/CppStringToken.cpp
@@ -1,72 +1,73 @@
 /**
- * <h1>PascalStringToken</h1>
+ * <h1>CppStringToken</h1>
  *
- * <p> Pascal string tokens.</p>
+ * <p> Cpp string tokens.</p>
  *
- * <p>Copyright (c) 2017 by Ronald Mak</p>
+ * <p>Copyright (c) 2017 by Ronald Mak & Under Pressure</p>
  * <p>For instructional purposes only.  No warranties.</p>
  */
+#include "CppStringToken.h"
+
 #include <string>
-#include "PascalStringToken.h"
-#include "../PascalError.h"
+#include "../CppError.h"
 
-namespace wci { namespace frontend { namespace pascal { namespace tokens {
-
-using namespace std;
-using namespace wci::frontend;
-using namespace wci::frontend::pascal;
-
-PascalStringToken::PascalStringToken(Source *source) throw (string)
-    : PascalToken(source)
-{
-    extract();
-}
-
-void PascalStringToken::extract() throw (string)
-{
-    string value_str = "";
-
-    char current_ch = next_char();  // consume initial quote
-    text += "'";
-
-    // Get string characters.
-    do
+namespace wci { namespace frontend { namespace Cpp { namespace tokens {
+    
+    using namespace std;
+    using namespace wci::frontend;
+    using namespace wci::frontend::Cpp;
+    
+    CppStringToken::CppStringToken(Source *source) throw (string)
+    : CppToken(source)
     {
-        // Replace any whitespace character with a blank.
-        if (isspace(current_ch)) current_ch = ' ';
-
-        if ((current_ch != '\'') && (current_ch != EOF))
+        extract();
+    }
+    
+    void CppStringToken::extract() throw (string)
+    {
+        string value_str = "";
+        
+        char current_ch = next_char();  // consume initial quote
+        text += "'";
+        
+        // Get string characters.
+        do
         {
-            text += current_ch;
-            value_str  += current_ch;
-            current_ch = next_char();  // consume character
-        }
-
-        // Quote?  Each pair of adjacent quotes represents a single-quote.
+            // Replace any whitespace character with a blank.
+            if (isspace(current_ch)) current_ch = ' ';
+            
+            if ((current_ch != '\'') && (current_ch != EOF))
+            {
+                text += current_ch;
+                value_str  += current_ch;
+                current_ch = next_char();  // consume character
+            }
+            
+            // Quote?  Each pair of adjacent quotes represents a single-quote.
+            if (current_ch == '\'')
+            {
+                while ((current_ch == '\'') && (peek_char() == '\''))
+                {
+                    text += "''";
+                    value_str  += current_ch;  // append single-quote
+                    current_ch = next_char();  // consume pair of quotes
+                    current_ch = next_char();
+                }
+            }
+        } while ((current_ch != '\'') && (current_ch != Source::END_OF_FILE));
+        
         if (current_ch == '\'')
         {
-            while ((current_ch == '\'') && (peek_char() == '\''))
-            {
-                text += "''";
-                value_str  += current_ch;  // append single-quote
-                current_ch = next_char();  // consume pair of quotes
-                current_ch = next_char();
-            }
+            next_char();  // consume final quote
+            text += '\'';
+            type = (TokenType) CppT_STRING;
+            value = value_str;
         }
-    } while ((current_ch != '\'') && (current_ch != Source::END_OF_FILE));
-
-    if (current_ch == '\'')
-    {
-        next_char();  // consume final quote
-        text += '\'';
-        type = (TokenType) PT_STRING;
-        value = value_str;
+        else
+        {
+            type = (TokenType) CppT_ERROR;
+            value = (int) UNEXPECTED_EOF;
+        }
     }
-    else
-    {
-        type = (TokenType) PT_ERROR;
-        value = (int) UNEXPECTED_EOF;
-    }
-}
-
-}}}}  // namespace wci::frontend::pascal::tokens
+    
+}}}}  // namespace wci::frontend::Cpp::tokens
