7bb241b117b63611496ac8d6fb402be7671e2463
Alejandro Perez <alex2721ap@gmail.com>
2018-09-18 18:35:02 -0700

    Added the bones to When statment Parser

---
+++ Chapter8/wci/frontend/pascal/parsers/WhenStatementParser.cpp
@@ -0,0 +1,35 @@
+/*
+ * WhenStatementParser.cpp
+ *
+ *  Created on: Sep 18, 2018
+ *      Author: aleja
+ */
+
+#include <string>
+#include <set>
+#include "IfStatementParser.h"
+#include "WhenStatementParser.h"
+#include "StatementParser.h"
+#include "AssignmentStatementParser.h"
+#include "ExpressionParser.h"
+#include "../PascalParserTD.h"
+#include "../PascalToken.h"
+#include "../PascalError.h"
+#include "../../Token.h"
+#include "../../../intermediate/ICodeNode.h"
+#include "../../../intermediate/ICodeFactory.h"
+#include "../../../intermediate/icodeimpl/ICodeNodeImpl.h"
+
+
+namespace wci { namespace frontend { namespace pascal { namespace parsers {
+
+using namespace std;
+using namespace wci::frontend::pascal;
+using namespace wci::intermediate;
+using namespace wci::intermediate::icodeimpl;
+
+
+
+
+}}}}
+
---
+++ Chapter8/wci/frontend/pascal/parsers/WhenStatementParser.h
@@ -0,0 +1,42 @@
+/*
+ * WhenStatementParser.h
+ *
+ *  Created on: Sep 18, 2018
+ *      Author: aleja
+ */
+
+#ifndef WCI_FRONTEND_PASCAL_PARSERS_WHENSTATEMENTPARSER_H_
+#define WCI_FRONTEND_PASCAL_PARSERS_WHENSTATEMENTPARSER_H_
+
+#include <string>
+#include <set>
+#include "StatementParser.h"
+#include "../../Token.h"
+
+namespace wci { namespace frontend { namespace pascal { namespace parsers {
+
+using namespace std;
+using namespace wci::frontend;
+using namespace wci::frontend::pascal;
+using namespace wci::intermediate;
+
+class WhenStatementParser : public StatementParser
+{
+public:
+
+
+
+
+private:
+
+
+
+};
+
+
+}}}}
+
+
+
+
+#endif /* WCI_FRONTEND_PASCAL_PARSERS_WHENSTATEMENTPARSER_H_ */
