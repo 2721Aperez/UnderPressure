9a22aa20dca3a0bad5482412643acbe3b4cc5399
Alejandro Perez <alex2721ap@gmail.com>
2018-09-25 20:08:51 -0700

    KINDA WORKS

--- Chapter8/wci/frontend/pascal/parsers/WhileStatementParser.cpp
+++ Chapter8/wci/frontend/pascal/parsers/WhileStatementParser.cpp
@@ -1,15 +1,16 @@
-/**
- * <h1>WhileStatementParser</h1>
+/*
+ * WhenStatementParser.cpp
  *
- * <p>Parse a Pascal WHILE statement.</p>
- *
- * <p>Copyright (c) 2017 by Ronald Mak</p>
- * <p>For instructional purposes only.  No warranties.</p>
+ *  Created on: Sep 18, 2018
+ *      Author: aleja
  */
+
 #include <string>
 #include <set>
-#include "WhileStatementParser.h"
+#include "IfStatementParser.h"
+#include "WhenStatementParser.h"
 #include "StatementParser.h"
+#include "AssignmentStatementParser.h"
 #include "ExpressionParser.h"
 #include "../PascalParserTD.h"
 #include "../PascalToken.h"
@@ -19,79 +20,153 @@
 #include "../../../intermediate/ICodeFactory.h"
 #include "../../../intermediate/icodeimpl/ICodeNodeImpl.h"
 
+
 namespace wci { namespace frontend { namespace pascal { namespace parsers {
 
-using namespace std;
-using namespace wci::frontend::pascal;
-using namespace wci::intermediate;
-using namespace wci::intermediate::icodeimpl;
+    using namespace std;
+    using namespace wci::frontend::pascal;
+    using namespace wci::intermediate;
+    using namespace wci::intermediate::icodeimpl;
 
-bool WhileStatementParser::INITIALIZED = false;
+    bool WhenStatementParser::INITIALIZED = false;
 
-set<PascalTokenType> WhileStatementParser::DO_SET;
+    set<PascalTokenType> WhenStatementParser::ARROW_SET;
 
-void WhileStatementParser::initialize()
-{
-    if (INITIALIZED) return;
-
-    DO_SET = StatementParser::STMT_START_SET;
-    DO_SET.insert(PascalTokenType::DO);
-
-    set<PascalTokenType>::iterator it;
-    for (it  = StatementParser::STMT_FOLLOW_SET.begin();
-         it != StatementParser::STMT_FOLLOW_SET.end();
-         it++)
+    void WhenStatementParser::initialize()
     {
-        DO_SET.insert(*it);
+        if (INITIALIZED) return;
+
+        ARROW_SET = StatementParser::STMT_START_SET;
+        ARROW_SET.insert(PascalTokenType::ARROW);
+
+        set<PascalTokenType>::iterator it;
+        for (it  = StatementParser::STMT_FOLLOW_SET.begin();
+             it != StatementParser::STMT_FOLLOW_SET.end();
+             it++)
+        {
+            ARROW_SET.insert(*it);
+        }
+
+        INITIALIZED = true;
     }
 
-    INITIALIZED = true;
-}
-
-WhileStatementParser::WhileStatementParser(PascalParserTD *parent)
+    WhenStatementParser::WhenStatementParser(PascalParserTD *parent)
     : StatementParser(parent)
-{
-    initialize();
-}
-
-ICodeNode *WhileStatementParser::parse_statement(Token *token) throw (string)
-{
-    token = next_token(token);  // consume the WHILE
-
-    // Create LOOP, TEST, and NOT nodes.
-    ICodeNode *loop_node =
-            ICodeFactory::create_icode_node((ICodeNodeType) NT_LOOP);
-    ICodeNode *test_node =
-            ICodeFactory::create_icode_node((ICodeNodeType) NT_TEST);
-    ICodeNode *not_node =
-            ICodeFactory::create_icode_node((ICodeNodeType) NT_NOT);
-
-    // The LOOP node adopts the TEST node as its first child.
-    // The TEST node adopts the NOT node as its only child.
-    loop_node->add_child(test_node);
-    test_node->add_child(not_node);
-
-    // Parse the expression.
-    // The NOT node adopts the expression subtree as its only child.
-    ExpressionParser expression_parser(this);
-    not_node->add_child(expression_parser.parse_statement(token));
-
-    // Synchronize at the DO.
-    token = synchronize(DO_SET);
-    if (token->get_type() == (TokenType) PT_DO)
     {
-        token = next_token(token);  // consume the DO
-    }
-    else {
-        error_handler.flag(token, MISSING_DO, this);
+        initialize();
     }
 
-    // Parse the statement.
-    // The LOOP node adopts the statement subtree as its second child.
-    StatementParser statement_parser(this);
-    loop_node->add_child(statement_parser.parse_statement(token));
+    ICodeNode *WhenStatementParser::parse_statement(Token *token) throw(string)
+	{
+    	token = next_token(token); //Consume When
 
-    return loop_node;
-}
+    	//Create When Node
+    	ICodeNode *when_node = ICodeFactory::create_icode_node((ICodeNodeType) NT_IF);
 
-}}}}  // namespace wci::frontend::pascal::parsers
+
+    	//Parse the expression
+        ExpressionParser expression_parser(this);
+        when_node->add_child(expression_parser.parse_statement(token));
+
+    	//synchronize token =>
+        token = synchronize(ARROW_SET);
+        if(token->get_type() == (TokenType) PT_ARROW)
+        {
+            token = next_token(token); //consume the arrow
+        }
+        else
+        {
+            error_handler.flag(token, MISSING_ARROW, this);
+        }
+
+        StatementParser statement_parser(this);
+        when_node->add_child(statement_parser.parse_statement(token));
+        token = current_token();
+
+        // Look for the semicolon between CASE branches.
+        if (token->get_type() == (TokenType) PT_SEMICOLON)
+        {
+            token = next_token(token);  // consume the ;
+        }
+
+
+        while((token != nullptr) && (token->get_type() != (TokenType) PT_OTHERWISE))
+        {
+//        	ICodeNode *when_node = ICodeFactory::create_icode_node((ICodeNodeType) NT_IF);
+//        	ExpressionParser expression_parser(this);
+        	when_node->add_child(expression_parser.parse_statement(token));
+
+        	token = synchronize(ARROW_SET);
+            if(token->get_type() == (TokenType) PT_ARROW)
+            {
+                token = next_token(token); //consume the arrow
+            }
+            else
+            {
+            	error_handler.flag(token, MISSING_ARROW, this);
+            }
+
+            StatementParser statement_parser(this);
+            when_node->add_child(statement_parser.parse_statement(token));
+            token = current_token();
+
+            if (token->get_type() == (TokenType) PT_SEMICOLON)
+            {
+                token = next_token(token);  // consume the ;
+
+            }
+            else
+            {
+            	error_handler.flag(token, MISSING_SEMICOLON, this);
+            }
+
+
+        }
+
+        if (token->get_type() == (TokenType) PT_OTHERWISE)
+        {
+            token = next_token(token);  // consume the THEN
+
+            // Parse the Otherwise statement.
+            // The when node adopts the statement subtree as its third child.
+            when_node->add_child(statement_parser.parse_statement(token));
+        }
+        else
+        {
+        	error_handler.flag(token, MISSING_OTHERWISE, this);
+        }
+    	token = synchronize(ARROW_SET);
+        if(token->get_type() == (TokenType) PT_ARROW)
+        {
+            token = next_token(token); //consume the arrow
+        }
+        else
+        {
+        	error_handler.flag(token, MISSING_ARROW, this);
+        }
+
+        //StatementParser statement_parser(this);
+        when_node->add_child(statement_parser.parse_statement(token));
+        token = current_token();
+
+        if (token->get_type() == (TokenType) PT_SEMICOLON)
+        {
+            token = next_token(token);  // consume the ;
+        }
+
+        // Look for the END token.
+        if (token->get_type() == (TokenType) PT_END)
+        {
+            token = next_token(token);  // consume END
+        }
+        else
+        {
+            error_handler.flag(token, MISSING_END, this);
+        }
+
+        return when_node;
+	}
+
+
+
+}}}}
